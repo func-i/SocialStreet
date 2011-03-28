e9fcb39b85a900f0808807aa1224af4f3104dffc
Khurram Virani <kvirani@gmail.com>
2011-03-28 11:36:14 -0400

    Initial Rails skeleton. Using rails 3.0.5, PG for db, and no test unit framework at the moment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialStreet::Application.config.secret_token = '18f6a77fc51346a83b91467f48f4b3fe87fe0a8b6239dd8acf140d218f6dec50b5a15a939d89e0915be825c956f000c0058b5a26aca850cece296b1e8584fe54'
