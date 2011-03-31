5ba67a19d94e2c6fa36da9993079286a7d6aea4d
Khurram Virani <kvirani@gmail.com>
2011-03-31 13:55:10 -0400

    Fix SQL error

--- app/models/event.rb
+++ app/models/event.rb
@@ -26,15 +26,15 @@ class Event < ActiveRecord::Base
     where(:held_on_day_of_week => days)
   }
   scope :on_days_or_in_date_range, lambda {|days, from_date, to_date|
-    queries = []
+    queries = [] # the date range queries are inside an OR so we build them like this - Don't know of a better way (yet) - KV
     if from_date && from_date = Time.zone.parse(from_date)
       queries << "events.held_on >= '#{from_date.beginning_of_day.to_s(:db)}'"
     end
     if to_date && to_date = Time.zone.parse(to_date)
       queries << "events.held_on <= '#{to_date.end_of_day.to_s(:db)}'"
     end
-
-    where("events.held_on_day_of_week IN (?) OR (#{queries.join(" AND ")})", days)
+    date_query = "OR (#{queries.join(" AND ")})" unless queries.blank? # Don't want "OR ()" showing up in there (SQL Error)
+    where("events.held_on_day_of_week IN (?) #{date_query}", days)
   }
 
   def location_address
