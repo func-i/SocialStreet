767528286fa817ec846d3264c8dd5abc776d74c5
Khurram Virani <kvirani@gmail.com>
2011-04-28 16:29:02 -0400

    more work on rsvp invitations

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,7 +1,15 @@
 class InvitationsController < ApplicationController
 
+
+  # only support nested action that expects event and rsvp IDs
   def new
+    @event = Event.find params[:event_id].to_i
+    @rsvp = @event.rsvps.find params[:rsvp_id].to_i
     @connections = current_user.connections.most_relevant_first.limit(30).all
+  end
+
+  def create
+    render :text => params.inspect
   end
 
 end
