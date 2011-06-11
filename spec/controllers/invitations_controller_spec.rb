198118f3c0c97027654c3959f04814bf810ea126
Sailias <jtsseneca@hotmail.com>
2011-06-11 18:43:49 -0400

    run with rspec spec/controllers/invitations_controller_spec.rb

---
+++ spec/controllers/invitations_controller_spec.rb
@@ -0,0 +1,112 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+include Devise::TestHelpers # to give your spec access to helpers
+
+describe InvitationsController do
+  before(:each) do
+    @event = Factory.create :event
+    @user = @event.user
+    @rsvp = @event.rsvps.first
+
+    @other_users = []
+    50.times{@other_users << Factory.create(:user)}
+
+    @connections = @other_users.each{|ou| @user.connections.create(:to_user=>ou)}
+
+    request.env['warden'] = mock(Warden, :authenticate => @user,
+      :authenticate! => @user)
+  end
+
+  describe "GET /new WITHOUT PARAMS" do
+    before { get :new, :event_id => @event.id, :rsvp_id=>@rsvp.id}
+
+    it "should have the user setup" do
+      assigns(:current_user).should == @user
+    end
+
+
+    it "should setup @event" do
+      assigns(:event).should == @event
+    end
+
+    it "should setup @rsvp" do
+      assigns(:rsvp).should == @rsvp
+    end
+
+    it "should have setup @users" do
+      assigns(:users).should_not be_nil
+    end
+
+    it "should have set @per_page to 10" do
+      assigns[:per_page].should == 10
+    end
+
+    it "should have offset of 0" do
+      assigns[:offset].should == 0
+    end
+
+    it "should have a total count equal to User.count-1" do
+      assigns[:total_count].should == User.where("users.id <> ?", @user.id).count
+    end
+
+    it "should have User.count / 10 number of pages" do
+      assigns[:num_pages].should == (assigns[:total_count].to_f / assigns[:per_page].to_f).ceil
+    end
+
+    it "should have @users with a count of 10" do
+      assigns[:users].all.size.should == 10
+    end
+    
+  end
+
+  describe "GET /new WITH PARAMS" do
+    
+    it "should have only user with exact search" do
+      get :new, :event_id => @event.id, :rsvp_id=>@rsvp.id, :user_search=>@other_users[10].email
+      assigns[:users].all.size.should == 1
+    end
+
+    it "should have total_count of all users except current_user when search is 'Person'" do
+      get :new, :event_id => @event.id, :rsvp_id=>@rsvp.id, :user_search=>'Person'
+      assigns[:total_count].should ==  User.where("users.id <> ?", @user.id).count
+    end
+
+    it "should return less than all users when search is 'Person \\w'" do
+      get :new, :event_id => @event.id, :rsvp_id=>@rsvp.id, :user_search=>"Person #{@other_users[10].last_name[0..-2]}"
+      assigns[:total_count].should < User.where("users.id <> ?", @user.id).count
+    end
+
+    it "should have an offset of 10 when page = 2" do
+      get :new, :event_id => @event.id, :rsvp_id=>@rsvp.id, :page=>2
+      assigns[:offset].should == 10
+    end
+
+  end
+
+  describe "GET /change WITH PARAMS" do
+
+    it "should have only user with exact search" do
+      get :change, :event_id => @event.id, :rsvp_id=>@rsvp.id, :user_search=>@other_users[10].email
+      assigns[:users].all.size.should == 1
+    end
+
+    it "should have total_count of all users except current_user when search is 'Person'" do
+      get :change, :event_id => @event.id, :rsvp_id=>@rsvp.id, :user_search=>'Person'
+      assigns[:total_count].should ==  User.where("users.id <> ?", @user.id).count
+    end
+
+    it "should return less than all users when search is 'Person \\w'" do
+      get :change, :event_id => @event.id, :rsvp_id=>@rsvp.id, :user_search=>"Person #{@other_users[10].last_name[0..-2]}"
+      assigns[:total_count].should < User.where("users.id <> ?", @user.id).count
+    end
+
+    it "should have an offset of 10 when page = 2" do
+      get :change, :event_id => @event.id, :rsvp_id=>@rsvp.id, :page=>2
+      assigns[:offset].should == 10
+    end
+
+  end
+  
+end
---
+++ spec/factories.rb
@@ -0,0 +1,36 @@
+Factory.define :user do |user|
+  user.first_name 'Person'
+  user.sequence(:last_name) {|n| "#{n}"}
+  user.sequence(:email) {|n| "email#{n}@socialstreet.com"}
+end
+
+Factory.define :event_type do |et|
+  et.name "Ball Hockey"
+end
+
+Factory.define :event do |event|
+  event.name "New Event"
+  event.association :user
+  event.association :searchable
+  event.after_create{|e| Factory(:rsvp, :event=>e)}
+end
+
+Factory.define :rsvp do |rsvp|
+  rsvp.association :user
+  rsvp.association :event
+  rsvp.status "Attending"
+end
+
+Factory.define :location do |loc|
+  loc.text "A new location"
+end
+
+Factory.define :searchable do |sea|
+  sea.association :location
+  sea.after_create{|s| Factory(:searchable_date_range, :searchable=>s)}
+end
+
+Factory.define :searchable_date_range do |sdr|
+  sdr.starts_at "#{5.days.from_now}"
+end
+
