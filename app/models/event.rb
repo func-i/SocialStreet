24f066ccea7c78faebade824ab7780b97b264ace
Sailias <jtsseneca@hotmail.com>
2011-06-18 14:47:15 -0400

    removing posting through friends walls

--- app/models/event.rb
+++ app/models/event.rb
@@ -22,10 +22,13 @@ class Event < ActiveRecord::Base
 
   before_validation :set_default_title
   before_create :build_initial_rsvp
-
-  after_create :post_to_facebook_walls
+  
   #before_destroy :validate_destroy  
   after_create :make_searchable_explorable
+
+  after_create {|record| record.user.post_to_facebook_wall(
+      :message => "SocialStreet Event Created: #{record.name}"
+    ) if record.facebook }
 
 
   validates :name, :presence => true, :length => { :maximum => 60 }
@@ -234,15 +237,17 @@ class Event < ActiveRecord::Base
   end
 
   def post_to_facebook_walls
-    user.post_to_facebook_wall(:message => "SocialStreet Event Created: #{self.name}")
 
-    # => Post to the users friends walls    
-    user.facebook_user.friends.each do |fb_friend|     
-      fb_friend.feed!(
-        :message => "Your friend #{user.name} has created a new SocialStreet Event: #{self.name}",
-        :link => nil
-      )
-    end if user.facebook_user
+    if facebook
+
+      # => Post to the users friends walls
+      user.facebook_user.friends.each do |fb_friend|
+        fb_friend.feed!(
+          :message => "Your friend #{user.name} has created a new SocialStreet Event: #{self.name}",
+          :link => nil
+        )
+      end if user.facebook_user
+    end
     
   end
   
