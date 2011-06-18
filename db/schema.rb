7c07d7be7f7e4b1599aa6cb85d16932f7c13c0ce
Sailias <jtsseneca@hotmail.com>
2011-06-18 10:50:29 -0400

    committing schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20110615163022) do
 
   create_table "actions", :force => true do |t|
