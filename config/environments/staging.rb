a2f985597d4f82b820432ab653c206543e76d9e3
Khurram Virani <kvirani@gmail.com>
2011-06-21 14:35:38 -0400

    sendmail -> gmail smtp for STAGING

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -37,12 +37,16 @@ SocialStreet::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # config.action_mailer.sendmail_settings = {
-  #   :location => '/usr/sbin/sendmail',
-  #   :arguments => '-i -t'
-  # }
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'baci.lindsaar.net',
+    :user_name            => 'notify@socialstreet.com',
+    :password             => 'ssP@ssw0rd',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
