df449598b66fce919b9dc67d9615a222190eb5e5
Khurram Virani <kvirani@gmail.com>
2011-06-21 11:07:23 -0400

    upgraded to rails 3.0.9 and added rediculous speed to the dev server but including another magical gem

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.7'
+gem 'rails', '3.0.9'
 gem 'rake', '0.9.2'
 
 # Bundle edge Rails instead:
@@ -32,6 +32,9 @@ gem 'json'
 
 group :development do
   gem 'thin'
+
+  # Boosts dev server response time significantly but if you have refresh issues, remove this gem
+  gem 'rails-dev-boost', :git => 'git://github.com/thedarkone/rails-dev-boost.git', :require => 'rails_development_boost'
 end
 
 group :test, :development do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110615163022) do
+ActiveRecord::Schema.define(:version => 20110619033159) do
 
   create_table "actions", :force => true do |t|
     t.integer  "event_id"
@@ -148,13 +148,14 @@ ActiveRecord::Schema.define(:version => 20110615163022) do
   add_index "locations", ["user_id"], :name => "index_locations_on_user_id"
 
   create_table "rsvps", :force => true do |t|
-    t.integer  "event_id",                         :null => false
-    t.integer  "user_id",                          :null => false
+    t.integer  "event_id",                              :null => false
+    t.integer  "user_id",                               :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "status"
-    t.boolean  "administrator", :default => false
+    t.boolean  "administrator",      :default => false
     t.boolean  "waiting"
+    t.boolean  "posted_to_facebook", :default => false
   end
 
   add_index "rsvps", ["event_id"], :name => "index_rsvps_on_event_id"
@@ -237,9 +238,9 @@ ActiveRecord::Schema.define(:version => 20110615163022) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                         :default => "", :null => false
-    t.string   "encrypted_password",             :limit => 128, :default => "", :null => false
-    t.string   "password_salt",                                 :default => "", :null => false
+    t.string   "email",                                         :default => "",    :null => false
+    t.string   "encrypted_password",             :limit => 128, :default => "",    :null => false
+    t.string   "password_salt",                                 :default => "",    :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
@@ -258,6 +259,7 @@ ActiveRecord::Schema.define(:version => 20110615163022) do
     t.string   "comment_notification_frequency"
     t.string   "fb_uid"
     t.string   "photo"
+    t.boolean  "fb_friends_imported",                           :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
