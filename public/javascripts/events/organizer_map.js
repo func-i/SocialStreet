a6da50a49532a41cf5b67043d1494020b74c59c5
Sailias <jtsseneca@hotmail.com>
2011-07-07 22:23:49 -0400

    [#15332255] MarkerClustering

--- public/javascripts/events/organizer_map.js
+++ public/javascripts/events/organizer_map.js
@@ -5,145 +5,151 @@ var markers = [];
 var dropPinState = false; // true if button to drop pins is selected
 var selectedMarker = null;
 var controlUI = null;
+var mc;
 
 // for testing only;
 var toronto = new google.maps.LatLng(43.7427662, -79.3922001);
 
 $(function() {
-  infoWindow = new google.maps.InfoWindow();
-  geocoder = new google.maps.Geocoder();
-  var myOptions = {
-    zoom: 12,
-    center: toronto,
-    mapTypeControl: false,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    streetViewControl: false
-  };
-  map = new google.maps.Map(document.getElementById("event-location-map"), myOptions);
-  // Create the DIV to hold the control and call the DropPinControl() constructor
-  // passing in this DIV.
-  var homeControlDiv = document.createElement('DIV');
-  var homeControl = new DropPinControl(homeControlDiv, map);
+    infoWindow = new google.maps.InfoWindow();
+    geocoder = new google.maps.Geocoder();
+    var myOptions = {
+        zoom: 12,
+        center: toronto,
+        mapTypeControl: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        streetViewControl: false
+    };
+    map = new google.maps.Map(document.getElementById("event-location-map"), myOptions);
 
-  homeControlDiv.index = 1;
-  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(homeControlDiv);
+    // Set the MarkerClusterer here
+    var mcOptions = {gridSize: 50, maxZoom: 12};
+    mc = new MarkerClusterer(map, markers, mcOptions)
 
-  google.maps.event.addListener(map, 'click', function(event) {
-    if (dropPinState) {
-      disableDropPinState();
-      placeMarker(event.latLng, $('#location-name-field').val());
-    }
-  });
+    // Create the DIV to hold the control and call the DropPinControl() constructor
+    // passing in this DIV.
+    var homeControlDiv = document.createElement('DIV');
+    var homeControl = new DropPinControl(homeControlDiv, map);
+
+    homeControlDiv.index = 1;
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(homeControlDiv);
+
+    google.maps.event.addListener(map, 'click', function(event) {
+        if (dropPinState) {
+            disableDropPinState();
+            placeMarker(event.latLng, $('#location-name-field').val());
+        }
+    });
 });
 
 function disableDropPinState() {
-  dropPinState = false;
-  controlUI.style.backgroundColor = 'white';
+    dropPinState = false;
+    controlUI.style.backgroundColor = 'white';
 }
 
 function placeMarker(location, title) {
 
-  var marker = new google.maps.Marker({
-    map: map,
-    position: location,
-    title: title,
-    draggable:true,
-    animation: google.maps.Animation.DROP
-  });
+    var marker = new google.maps.Marker({
+        map: map,
+        position: location,
+        title: title,
+        draggable:true,
+        animation: google.maps.Animation.DROP
+    });
 
-  google.maps.event.addListener(marker, 'click', function() {
-    selectMarker(marker, true);
-  });
-  markers.push(marker);
+    google.maps.event.addListener(marker, 'click', function() {
+        selectMarker(marker, true);
+    });
+    markers.push(marker);
 
-  selectMarker(marker);
+    selectMarker(marker);
 
 //map.setCenter(location);
 }
 
 function DropPinControl(controlDiv, map) {
 
-  // Set CSS styles for the DIV containing the control
-  // Setting padding to 5 px will offset the control
-  // from the edge of the map
-  controlDiv.style.padding = '5px';
+    // Set CSS styles for the DIV containing the control
+    // Setting padding to 5 px will offset the control
+    // from the edge of the map
+    controlDiv.style.padding = '5px';
 
-  // Set CSS for the control border
-  controlUI = document.createElement('DIV');
-  controlUI.style.backgroundColor = 'white';
-  controlUI.style.borderStyle = 'solid';
-  controlUI.style.borderWidth = '2px';
-  controlUI.style.cursor = 'pointer';
-  controlUI.style.textAlign = 'center';
-  controlUI.title = 'Click to specify the location manually';
-  controlDiv.appendChild(controlUI);
+    // Set CSS for the control border
+    controlUI = document.createElement('DIV');
+    controlUI.style.backgroundColor = 'white';
+    controlUI.style.borderStyle = 'solid';
+    controlUI.style.borderWidth = '2px';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.textAlign = 'center';
+    controlUI.title = 'Click to specify the location manually';
+    controlDiv.appendChild(controlUI);
 
-  // Set CSS for the control interior
-  var controlText = document.createElement('DIV');
-  controlText.style.fontFamily = 'Arial,sans-serif';
-  controlText.style.fontSize = '12px';
-  controlText.style.paddingLeft = '4px';
-  controlText.style.paddingRight = '4px';
-  controlText.innerHTML = 'Drop Pin';
-  controlUI.appendChild(controlText);
+    // Set CSS for the control interior
+    var controlText = document.createElement('DIV');
+    controlText.style.fontFamily = 'Arial,sans-serif';
+    controlText.style.fontSize = '12px';
+    controlText.style.paddingLeft = '4px';
+    controlText.style.paddingRight = '4px';
+    controlText.innerHTML = 'Drop Pin';
+    controlUI.appendChild(controlText);
 
-  // Setup the click event listeners: simply set the map to Chicago
-  google.maps.event.addDomListener(controlUI, 'click', function() {
-    if (dropPinState) {
-      disableDropPinState();
-    } else {
-      dropPinState = true;
-      controlUI.style.backgroundColor = '#9999DD';
-    }
+    // Setup the click event listeners: simply set the map to Chicago
+    google.maps.event.addDomListener(controlUI, 'click', function() {
+        if (dropPinState) {
+            disableDropPinState();
+        } else {
+            dropPinState = true;
+            controlUI.style.backgroundColor = '#9999DD';
+        }
 
-  });
+    });
 }
 
 function clearMarkers() {
-  $.each(markers, function(index, marker) {
-    marker.setMap(null);
-  });
-  markers = [];
+    $.each(markers, function(index, marker) {
+        marker.setMap(null);
+    });
+    markers = [];
 }
 
 function searchLocations(e) {
-  var loc = e.target.value;
-  geocoder.geocode( {
-    'address': loc
-  }, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      clearMarkers()
-      $.each(results, function(index, result) {
-        placeMarker(result.geometry.location, result.formatted_address);
-      });
-      selectMarker(markers[0], false);
-    } else if (status == google.maps.GeocoderStatus.ZERO_RESULTS) {
+    var loc = e.target.value;
+    geocoder.geocode( {
+        'address': loc
+    }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            clearMarkers()
+            $.each(results, function(index, result) {
+                placeMarker(result.geometry.location, result.formatted_address);
+            });
+            selectMarker(markers[0], false);
+        } else if (status == google.maps.GeocoderStatus.ZERO_RESULTS) {
       
-      var sw = map.getBounds().getSouthWest();
-      var ne = map.getBounds().getNorthEast();
-      var sw_lat = sw.lat(), sw_lng = sw.lng();
-      var ne_lat = ne.lat(), ne_lng = ne.lng();
+            var sw = map.getBounds().getSouthWest();
+            var ne = map.getBounds().getNorthEast();
+            var sw_lat = sw.lat(), sw_lng = sw.lng();
+            var ne_lat = ne.lat(), ne_lng = ne.lng();
       
-      $.getJSON(locationSearchURL, {
-        query: loc,
-        sw_lat: sw_lat,
-        sw_lng: sw_lng,
-        ne_lat: ne_lat,
-        ne_lng: ne_lng
-      }, function(data, textStatus, jqHXR) {
-        if (data.length > 0) {
-          $.each(data, function(index, result) {
-            placeMarker(new google.maps.LatLng(result.latitude, result.longitude), result.text);
-          });
+            $.getJSON(locationSearchURL, {
+                query: loc,
+                sw_lat: sw_lat,
+                sw_lng: sw_lng,
+                ne_lat: ne_lat,
+                ne_lng: ne_lng
+            }, function(data, textStatus, jqHXR) {
+                if (data.length > 0) {
+                    $.each(data, function(index, result) {
+                        placeMarker(new google.maps.LatLng(result.latitude, result.longitude), result.text);
+                    });
+                } else {
+                    alert("No results found, please drop a pin to tell us where this is");
+                }
+            });
         } else {
-          alert("No results found, please drop a pin to tell us where this is");
+            alert("Geocode was not successful for the following reason: " + status);
         }
-      });
-    } else {
-      alert("Geocode was not successful for the following reason: " + status);
-    }
-  });
-  return false;
+    });
+    return false;
 }
 
 function updateMarkerTitle() {
@@ -151,50 +157,59 @@ function updateMarkerTitle() {
 }
 
 function selectMarker(marker, changeInputField) {
-  selectedMarker = marker;
+    selectedMarker = marker;
 
-  if (marker.getAnimation() == null) {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
+    if (marker.getAnimation() == null) {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            if (marker.getAnimation() == google.maps.Animation.BOUNCE) marker.setAnimation(null);
+        }, 740);
+    }
+
+    var html = "<table>" +
+    "Name: <input type='text' id='marker-name-field' value='"+marker.title+"'/>";
+
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+
+    var latlng = marker.getPosition();
+    $('#location-lat-field').val(latlng.lat());
+    $('#location-lng-field').val(latlng.lng());
+    if (changeInputField) $('#location-name-field').val(marker.title);
     setTimeout(function() {
-      if (marker.getAnimation() == google.maps.Animation.BOUNCE) marker.setAnimation(null);
-    }, 740);
-  }
-
-  var html = "<table>" +
-  "Name: <input type='text' id='marker-name-field' value='"+marker.title+"'/>";
-
-  infoWindow.setContent(html);
-  infoWindow.open(map, marker);
-
-  var latlng = marker.getPosition();
-  $('#location-lat-field').val(latlng.lat());
-  $('#location-lng-field').val(latlng.lng());
-  if (changeInputField) $('#location-name-field').val(marker.title);
-  setTimeout(function() {
-    $('#marker-name-field').focus();
-  }, 100);
+        $('#marker-name-field').focus();
+    }, 100);
 }
 
 // don't allow enter to submit form
 
 $('#location-name-field').keydown(function(e) {
-  if (e.keyCode == 13) {
-    e.stopPropagation();
-    searchLocations(e);
-    return false;
-  }
-  return true;
+    if (e.keyCode == 13) {
+        e.stopPropagation();
+        searchLocations(e);
+        return false;
+    }
+    return true;
 });
 
 
 $('#marker-name-field').live('keyup', function(e) {
-  $('#location-name-field').val(this.value);
-  selectedMarker.setTitle(this.value);
-  return true;
+    $('#location-name-field').val(this.value);
+    selectedMarker.setTitle(this.value);
+    return true;
 }).live('keydown', function(e) {
-  if (e.keyCode == 13) {
-    e.stopPropagation();
-    return false;
-  }
+    if (e.keyCode == 13) {
+        e.stopPropagation();
+        return false;
+    }
 });
 $('#location-name-field').change(searchLocations);
+
+function clearClusterMarkers() {
+    mc.clearMarkers();
+    markers = [];
+}
+function placeClusterMarkers(){
+    if(markers.length > 0)
+        mc.addMarkers(markers);
+}
