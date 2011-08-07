d7c1d8cd738a294ef284653e31da58c959410562
Jonathan Salis <jtsseneca@hotmail.com>
2011-08-07 02:57:38 -0400

    fixing pushing end date time for create event

--- public/javascripts/events/form.js
+++ public/javascripts/events/form.js
@@ -16,9 +16,13 @@ $(function() {
     $('.starts_at_time').change(function() {
         $('.starts_at_value').val($('#starts_at_calendar').val() + ' ' + $($('.starts_at_time')[1]).val() + ':' + $($('.starts_at_time')[2]).val());
                         
-        var stTime = new Date($('.starts_at_value').val());
-        var endTime = stTime.addHours(3);
+        var stTime = $('#starts_at_calendar').datepicker('getDate');
+        stTime.setHours($($('.starts_at_time')[1]).val());
+        stTime.setMinutes($($('.starts_at_time')[2]).val());
 
+        var endTime = new Date(stTime.getTime());
+        endTime.setHours(endTime.getHours() + 3);
+        
         $('#ends_at_calendar').val(endTime.format('yyyy-mm-dd'));
         $($('.ends_at_time')[1]).val(endTime.format('hh'));
         $($('.ends_at_time')[2]).val(endTime.format('MM'));
@@ -29,10 +33,4 @@ $(function() {
     $('.ends_at_time').change(function() {
         $('.ends_at_value').val($('#ends_at_calendar').val() + ' ' + $($('.ends_at_time')[1]).val() + ':' + $($('.ends_at_time')[2]).val());
     });
-
-    Date.prototype.addHours= function(h){
-        this.setHours(this.getHours()+h);
-        return this;
-    }
-
 });
