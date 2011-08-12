7da8c72f45d22f87a2e05c889e542cd497b9a3ae
Jonathan Salis <jtsseneca@hotmail.com>
2011-08-11 21:52:09 -0400

    speeding up facebook authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,11 @@ class SessionsController < Devise::SessionsController
 
   def new
     respond_to do |format|
-      format.js do
-        render :update do |page|
-          page.redirect_to '/auth/facebook'
-        end
-      end
+      format.js 
       format.html do
         redirect_to '/auth/facebook'
       end
-    end
-    
-    
+    end    
   end
+  
 end
