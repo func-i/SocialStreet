d8f31f07bcb9b26ce2f8c584f2627f4d3b3b0cd6
Khurram Virani <kvirani@gmail.com>
2011-08-27 09:27:13 -0400

    for some reason password_salt already exists. not sure why this migration existed..both my dev and prod got the same error when running migrations

--- db/migrate/20110805143136_add_password_salt_to_users.rb
+++ db/migrate/20110805143136_add_password_salt_to_users.rb
@@ -1,6 +1,6 @@
 class AddPasswordSaltToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :password_salt, :string
+#    add_column :users, :password_salt, :string
   end
 
   def self.down
