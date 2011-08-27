0bfd0166ca42686c37c7e4c47738aa09dfe358ec
Khurram Virani <kvirani@gmail.com>
2011-08-27 12:25:52 -0400

    Using rack logging middleware replacement to exclude /hb URL from being logged. Minor change to gemfile. Readme notes (work in progress)

---
+++ lib/ss/smart_rack_logger.rb
@@ -0,0 +1,17 @@
+class SmartRackLogger < Rails::Rack::Logger
+  def initialize(app, opts = {})
+    @app = app
+    @opts = opts
+    @opts[:silenced] ||= []
+  end
+
+  def call(env)
+    if env['X-SILENCE-LOGGER'] || @opts[:silenced].include?(env['PATH_INFO'])
+      Rails.logger.silence do
+        @app.call(env)
+      end
+    else
+      super(env)
+    end
+  end
+end
