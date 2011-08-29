bee58a3ceb495f31e3aeecb38b6a6f140b6d53a3
Josh Borts <jborts@gmail.com>
2011-08-28 21:53:10 -0400

    Fixed ie accept modal

--- app/controllers/event_types_controller.rb
+++ app/controllers/event_types_controller.rb
@@ -11,7 +11,7 @@ class EventTypesController < ApplicationController
         :label => "#{"<img height='35' width='35' src='" + et.image_path + "' /> " if et.image_path}<span style='vertical-align: top; position: relative; top: 10px; left: 10px;'>#{et.name}</span>",
         :value => et.name}
 
-      et.children.limit(5).each do |child_et|
+      et.synonym.children.each do |child_et|
         return_list << {
           :label => "<span style='margin-left:20px'>#{"<img height='25' width='25' src='" + child_et.image_path + "' /> " if child_et.image_path}<span style='vertical-align: top; position: relative; top: 2px; left: 5px;'>#{child_et.name}</span></span>",
           :value => child_et.name
