75dae533fee82be81cec955694d7bf4cddbea4be
Josh Borts <jborts@gmail.com>
2011-09-12 13:26:22 -0400

    Changed staging cap to work of develop branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :deploy_to, "/home/ubuntu/rails/staging.socialstreet.com"
-set :branch, "staging"
+set :branch, "develop"
 server "ec2-184-73-88-200.compute-1.amazonaws.com", :app, :web, :db, :primary => true
 
