5ef5fd1f07c41526d94b24010684705820b65a6c
Jonathan Salis <jtsseneca@hotmail.com>
2011-09-15 12:33:34 -0400

    removing layout from streetmeet of the week mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -119,7 +119,9 @@ class UserMailer < ActionMailer::Base
   end
 
   def streetmeet_of_the_week(email)
-    mail(:to => email, :subject => "Your StreetMeet of the week invitation")
+    mail(:to => email, :subject => "Your StreetMeet of the week invitation") do |format|
+      format.html {render :layout => false}
+    end
   end
 
 end
