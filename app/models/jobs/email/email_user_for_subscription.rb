39f4833feec1c81d66be4dbef0bae85ef95f4a2b
Josh Borts <jborts@gmail.com>
2011-09-17 11:51:04 -0400

    Fixing bug in instant subscriptions

--- app/models/jobs/email/email_user_for_subscription.rb
+++ app/models/jobs/email/email_user_for_subscription.rb
@@ -4,7 +4,7 @@ class Jobs::Email::EmailUserForSubscription
 
   def self.perform(subscription_id, action_id)
     subscription = SearchSubscription.find subscription_id
-    action = Action.where(:id => action_id)
+    action = Action.where(:id => action_id).first
     user = subscription.user
     
     email = UserMailer.subscription_instant_notice(subscription, action, user) if action
