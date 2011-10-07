965c9b553545c28078a5a406dba733f31fca48c8
Jonathan Salis <jtsseneca@hotmail.com>
2011-10-07 18:50:16 -0400

    preparing mass email for streetmeet of the week

--- app/models/jobs/email/email_streetmeet_of_the_week.rb
+++ app/models/jobs/email/email_streetmeet_of_the_week.rb
@@ -4,8 +4,9 @@ class Jobs::Email::EmailStreetmeetOfTheWeek
 
   # the "orig_action_id" is the action that was in the same chain as the new action represented by "action_id"
   def self.perform
-    users = User.where("email <> ''").last(483)
+    users = User.where("email <> ''")
 
+    puts users.size
     i=0
     users.each do |user|
       begin
