4e40a8ca57c9a040e0fa11dbf90864a476a604c7
Jonathan Salis <jtsseneca@hotmail.com>
2011-10-07 17:55:09 -0400

    Changing Streetmeet email subject

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -119,7 +119,7 @@ class UserMailer < ActionMailer::Base
   end
 
   def streetmeet_of_the_week(email)
-    mail(:to => email, :subject => "StreetMeet of the Week - FREE Blue Jays Game") do |format|
+    mail(:to => email, :subject => "StreetMeet of the Week - Open Bar Vodka Party") do |format|
       format.html {render :layout => false}
     end
   end
