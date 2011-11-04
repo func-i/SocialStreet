3e3c8272be433cdc7882ee87fc117bccd0cbd373
Jonathan Salis <jtsseneca@hotmail.com>
2011-11-04 13:01:47 -0400

    adding user_group list for administration

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,27 @@
 class GroupsController < ExploreBaseController
+
+  before_filter :load_group, :only => [:show, :edit, :update, :destroy, :show_members]
+
   def show
-    @group = Group.find params[:id]
     super_index
   end
 
   def apply_for_membership
     #TODO
   end
+
+  def search_user_groups
+    unless params[:keyword].blank?
+      @user_groups = UserGroup.search(params[:keyword])
+    else
+      @user_groups = UserGroup.all
+    end
+  end
+
+  protected
+
+  def load_group
+    @group = Group.find params[:id]
+  end
+
 end
