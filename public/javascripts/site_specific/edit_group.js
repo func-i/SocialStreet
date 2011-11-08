2f5c916818a19e65544be1da8773264c808f4382
Jonathan Salis <jtsseneca@hotmail.com>
2011-11-08 00:34:00 -0500

    updating deleting and updating groups as well as fixing the textarea resizing issue when the description or other fields is too long

--- public/javascripts/site_specific/edit_group.js
+++ public/javascripts/site_specific/edit_group.js
@@ -4,9 +4,14 @@ $(function() {
     cleanUpSelf = function(){
     }
 
-    resizeSelf = function() {        
+    resizeSelf = function() {
+       
     }
 
+    $.each($('#group_form textarea'), function(i, ele) {
+            $(ele).attr('rows', countLines(ele) > 1 ? countLines(ele) + 1 : 1);
+    });
+    
     $('#edit_group_contact_address').autoResize();
     $('#edit_group_description').autoResize();
 
@@ -20,15 +25,36 @@ $(function() {
     });
 
     $('.user-group-item').live('click', function() {
-        var $form = $(this).find('form').first();
-        $newForm = $form.clone();
-        $('#user_group_holder').html($newForm);
-        $newForm.removeClass('hidden');
+        var $holder = $('#user_group_holder');        
+        $holder.html('');
+        $.each($(this).find('form'), function(i, ele) {            
+            $holder.append($(ele).clone());
+        });
     });
 
-    $('.user-group-item img').live('click', function() {
-        $(this).closest('li').remove();
+    $('#user_group_holder .delete-img').live('click', function() {
+        if(confirm('Are you sure you want to remove this user from the group?')) {            
+            $(this).closest('form').submit();
+
+            var group_id = $(this).data('user-group');
+            var $userGroupLi = $('*[data-li-user-group=' + group_id + ']').first();
+            
+            $userGroupLi.fadeOut(2500, function() {
+                $(this).remove();
+            });
+        }
+    });
+
+    $('.update-event-img').live('click', function() {
         $(this).closest('form').submit();
     });
 })
+
+function countLines(area)
+{
+    // trim trailing return char if exists
+    var text = area.value;
+    var rows = Math.ceil(text.length /area.cols);
+    return rows
+}
 
