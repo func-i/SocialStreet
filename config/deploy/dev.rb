52195d13403cd74ca1b39e706b6ffaa96557602f
Josh Borts <jborts@gmail.com>
2011-11-09 11:20:40 -0500

    Handling no groups

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,5 +1,5 @@
 set :deploy_to, "/home/ubuntu/rails/staging.socialstreet.com"
-set :branch, "reborn"
+set :branch, "feature-groups"
 server "ec2-184-73-88-200.compute-1.amazonaws.com", :app, :web, :db, :primary => true
 
 after "deploy:update_code", "deploy:generate_assets" unless fetch(:quick_update, false)
