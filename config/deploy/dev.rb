cebd26d7912aaeed459aefa69e7017a66514d00d
Josh Borts <jborts@gmail.com>
2011-11-09 11:49:10 -0500

    setting dev deploy back to reborn

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,5 +1,5 @@
 set :deploy_to, "/home/ubuntu/rails/staging.socialstreet.com"
-set :branch, "feature-groups"
+set :branch, "reborn"
 server "ec2-184-73-88-200.compute-1.amazonaws.com", :app, :web, :db, :primary => true
 
 after "deploy:update_code", "deploy:generate_assets" unless fetch(:quick_update, false)
