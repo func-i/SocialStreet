5ee15ff492d667babe32c2bc354ed6a39334227f
Josh Borts <jborts@gmail.com>
2011-12-23 18:39:57 -0500

    Storing user list for chat in redis

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -3,18 +3,28 @@ class ChatRoomsController < ApplicationController
   before_filter :ss_authenticate_user!
 
   def show
-    @chat_room = ChatRoom.find params[:id]    
+    @chat_room = ChatRoom.find params[:id]
+
+    r = Redis.new
+    @chat_members = User.find r.smembers("cr_user_list_#{@chat_room.id}")
+    r.quit
+
     render :layout => false
   end
 
   def join
     @chat_room = ChatRoom.find params[:id]
+
+    r = Redis.new
+    r.sadd "cr_user_list_#{@chat_room.id}", current_user.id
+    r.quit
   end
 
   def leave
-    @chat_room = ChatRoom.find params[:id]       
+    @chat_room = ChatRoom.find params[:id]
+
+    r = Redis.new
+    r.srem "cr_user_list_#{@chat_room.id}", current_user.id
+    r.quit
   end
-
-  
-
 end
