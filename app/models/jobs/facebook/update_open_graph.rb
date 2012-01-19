581831715aa1ec3b2022a0662d3045207009f200
Josh Borts <jborts@gmail.com>
2012-01-19 14:27:07 -0500

    Needed a queue

--- app/models/jobs/facebook/update_open_graph.rb
+++ app/models/jobs/facebook/update_open_graph.rb
@@ -1,4 +1,6 @@
 class Jobs::Facebook::UpdateOpenGraph
+  @queue = :actions
+
   def self.perform(event_id)
     event = Event.find event_id
     Net::HTTP.post('http://graph.facebook.com/' + event_url(event), 'scrape=true')
