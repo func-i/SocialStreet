92ac2de41ac717f4b1a3b76f3c41315fdcf8042c
Josh Borts <jborts@gmail.com>
2012-01-23 01:32:48 -0500

    url shortner fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 
   def url_shortner(url)
-    url.gsub(/www\.socialstreet\.com/, 'socialstreet.com')#Remove www
-    url.gsub(/socialstreet\.com/, 'scl.st')#shorter to scl
-    url.gsub(/http:\\\\\scl\.st/, 'scl.st')#Remove http
+    url = url.gsub(/www\.socialstreet\.com/, 'socialstreet.com')#Remove www
+    url = url.gsub(/socialstreet\.com/, 'scl.st')#shorter to scl
+    url = url.gsub(/http:\\\\\scl\.st/, 'scl.st')#Remove http
     url.gsub(/scl\.st\/events\//, 'scl.st/e/')#Shorten events
   end
 
