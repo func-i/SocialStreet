da1fcac0c32c7bd6b5b6a9143604aa81adfe154e
Josh Borts <jborts@gmail.com>
2012-01-24 21:11:26 -0500

    Update open graph url helpers

--- app/models/jobs/facebook/update_open_graph.rb
+++ app/models/jobs/facebook/update_open_graph.rb
@@ -1,8 +1,11 @@
 class Jobs::Facebook::UpdateOpenGraph
+  include ActionController::UrlWriter
+  require 'UrlHelper'
+  
   @queue = :actions
 
   def self.perform(event_id)
     event = Event.find event_id
-    Net::HTTP.post('http://graph.facebook.com/' + event_url(event), 'scrape=true')
+    Net::HTTP.post('http://graph.facebook.com/' + event_url(event, :host => "www.socialstreet.com"), 'scrape=true')
   end
 end
