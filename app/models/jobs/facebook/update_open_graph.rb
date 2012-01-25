b1732abda807235a322723cfa2b87272323ff968
Josh Borts <jborts@gmail.com>
2012-01-24 21:52:25 -0500

    Open graph fixes I hope

--- app/models/jobs/facebook/update_open_graph.rb
+++ app/models/jobs/facebook/update_open_graph.rb
@@ -1,7 +1,10 @@
-class Jobs::Facebook::UpdateOpenGraph  
+class Jobs::Facebook::UpdateOpenGraph
+  require 'net/http'
+  
   @queue = :actions
 
   def self.perform(event_id)
-    Net::HTTP.post("http://graph.facebook.com/http://www.socialstreet.com/events/#{event_id}/?scrape=true")
+    uri = URI("http://graph.facebook.com/http://www.socialstreet.com/events/#{event_id}")
+    Net::HTTP.post_form(uri, 'scrape' => 'true')
   end
 end
